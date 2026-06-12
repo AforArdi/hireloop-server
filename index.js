a937bfc440710fa154dc43b96f49cc5ab4a64b70
AforArdi <binsbro36@gmail.com>
2026-06-12 20:20:22 +0600

    post req created for new job by recruiter

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const db = client.db('hireloop_db');
+        const jobCollection = db.collection('jobs');
+
+        app.post('/api/jobs', async (req, res) => {
+            const job = req.body;
+            const result = await jobCollection.insertOne(job);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
